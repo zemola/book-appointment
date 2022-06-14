3ade74e3ab112c118e5f446d3e67a164cb0429b5
esnonso <devnellynonso@gmail.com>
2022-06-14 22:20:02 +0100

    complete project

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,143 +1,196 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 import { Select } from "react-select-states-and-lga-in-nigeria";
 import "react-select-states-and-lga-in-nigeria/dist/index.css";
 
-const Form = props => {
-    const [email, setEmail] = useState("");
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
-    const [phone, setPhone] = useState("");
-    const [address, setAddress] = useState("");
-    const [state, setState] = useState("");
-    const [lga, setLga] = useState("");
-  
-    const emailChangeHandler = (e) => {
-      setEmail(e.target.value);
+import Logo from "../../images/blossom heart.png";
+import Success from "../../images/success.png";
+import "./Form.css";
+
+const Form = (props) => {
+  const [email, setEmail] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [state, setState] = useState("");
+  const [lga, setLga] = useState("");
+  const [gender, setGender] = useState("");
+  const [reason, setReason] = useState("");
+  const [sucess, showSucess] = useState(false);
+  const [error, setError] = useState("");
+
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const firstnameChangeHandler = (e) => {
+    setFirstname(e.target.value);
+  };
+
+  const lastnameChangeHandler = (e) => {
+    setLastname(e.target.value);
+  };
+
+  const phoneChangeHandler = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const addressChangeHandler = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const handleStateChange = (e) => {
+    setState(e.target.value);
+  };
+
+  const handleLgaChange = (e) => {
+    setLga(e.target.value);
+  };
+
+  const handleGenderChange = (e) => {
+    setGender(e.target.value);
+  };
+
+  const handleReasonChange = (e) => {
+    setReason(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setError("");
+    if (
+      email === "" ||
+      firstname === "" ||
+      lastname === "" ||
+      phone === "" ||
+      address === "" ||
+      state === "" ||
+      lga === "" ||
+      reason === "" ||
+      gender === ""
+    ) {
+      setError("select All inputs");
+      return;
+    }
+
+    const url =
+      "https://prod-60.westeurope.logic.azure.com:443/workflows/b0d994885dc74699b807f1fd36783576/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=lA3ZzpbXx6EphtUrghKki5bs-Tc2bjmuGOE-vNYqS50";
+    const formData = {
+      email: email,
+      firstname: firstname,
+      lastname: lastname,
+      phone: +phone,
+      reason: reason,
+      gender: gender,
+      address: `${address} ${lga} ${state}`,
     };
-  
-    const firstnameChangeHandler = (e) => {
-      setFirstname(e.target.value);
-    };
-  
-    const lastnameChangeHandler = (e) => {
-      setLastname(e.target.value);
-    };
-  
-    const phoneChangeHandler = (e) => {
-      setPhone(e.target.value);
-    };
-  
-    const addressChangeHandler = (e) => {
-      setAddress(e.target.value);
-    };
-  
-    const handleStateChange = (e) => {
-      setState(e.target.value);
-    };
-  
-    const handleLgaChange = (e) => {
-      setLga(e.target.value);
-    };
-  
-    const submitHandler = (e) => {
-      e.preventDefault();
-      const url = "https://prod-60.westeurope.logic.azure.com:443/workflows/b0d994885dc74699b807f1fd36783576/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=lA3ZzpbXx6EphtUrghKki5bs-Tc2bjmuGOE-vNYqS50"
-      const formData = {
-        email:email, firstname:firstname, lastname:lastname, phone:+phone} 
-      
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      })
-        .then((res) => console.log("submitted"))
-        .catch((err) => console.log("failed"));
-        
-    };
-    return (
-      <div className="App">
-        <div className="form">
-          <div className="aside">
-            <h4>Blossom Heart Hospital</h4>
-            <p>
-              In publishing and graphic design, Lorem ipsum is a placeholder text
-              commonly used to demonstrate the visual form of a document or a
-              typeface without relying on meaningful content. Lorem ipsum may be
-              used as a placeholder before final copy is available.
-            </p>
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => showSucess(true))
+      .catch((err) => console.log("failed"));
+  };
+  return (
+    <div className="form">
+      <div className="header">
+        <div className="logo-div-form">
+          <img src={Logo} className="logo" alt="" />
+          <h3>Blossom Heart Hospital</h3>
+        </div>
+        <button className="close" onClick={props.onClose}>
+          X
+        </button>
+      </div>
+      {!sucess && (
+        <form onSubmit={submitHandler}>
+          {error && <small>Please fill all inputs</small>}
+          <div className="single-input">
+            <label>Email</label>
+            <input
+              placeholder="email"
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={emailChangeHandler}
+            />
           </div>
-          <div>
-            <form onSubmit={submitHandler}>
-              <h4>Book an appointment</h4>
-              <div className="single-input">
-                <input
-                  placeholder="email"
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={email}
-                  onChange={emailChangeHandler}
-                />
-              </div>
-  
-              <div className="double-input">
-                <input
-                  placeholder="First name"
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  value={firstname}
-                  onChange={firstnameChangeHandler}
-                />
-                <input
-                  placeholder="Last name"
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  value={lastname}
-                  onChange={lastnameChangeHandler}
-                />
-              </div>
-  
-  
-              <div className="double-input">
-                <input
-                  placeholder="Phone Number"
-                  type="number"
-                  name="phone-number"
-                  id="phone-number"
-                  value={phone}
-                  onChange={phoneChangeHandler}
-                />
-                <input
-                  placeholder="Address"
-                  type="text"
-                  name="address"
-                  id="address"
-                  value={address}
-                  onChange={addressChangeHandler}
-                />
-              </div>
-  
-              <div className="single-input">
-                <Select
-                  state={state}
-                  lga={lga}
-                  changeState={handleStateChange}
-                  changeLga={handleLgaChange}
-                />
-              </div>
-  
-              <div className="single-input">
-                <label>Select Reason</label>
-                <select>
-                <option  >
-                  Select
+
+          <div className="double-input">
+            <div>
+              <label>First Name</label>
+              <input
+                placeholder="First name"
+                type="text"
+                name="first-name"
+                id="first-name"
+                value={firstname}
+                onChange={firstnameChangeHandler}
+              />
+            </div>
+
+            <div>
+              <label>Last Name</label>
+              <input
+                placeholder="Last name"
+                type="text"
+                name="last-name"
+                id="last-name"
+                value={lastname}
+                onChange={lastnameChangeHandler}
+              />
+            </div>
+          </div>
+
+          <div className="double-input">
+            <div>
+              <label>Phone number</label>
+              <input
+                placeholder="Phone Number"
+                type="number"
+                name="phone-number"
+                id="phone-number"
+                value={phone}
+                onChange={phoneChangeHandler}
+              />
+            </div>
+
+            <div>
+              <label>Address</label>
+              <input
+                placeholder="Address"
+                type="text"
+                name="address"
+                id="address"
+                value={address}
+                onChange={addressChangeHandler}
+              />
+            </div>
+          </div>
+
+          <div className="single-input">
+            <Select
+              state={state}
+              lga={lga}
+              changeState={handleStateChange}
+              changeLga={handleLgaChange}
+            />
+          </div>
+
+          <div className="double-input">
+            <div>
+              <label>Select Reason</label>
+              <select value={reason} onChange={handleReasonChange}>
+                <option>Select</option>
+                <option>
+                  General Sickness (malaria,typhoid, cough, e.t.c)
                 </option>
-                <option>General Sickness (malaria,typhoid, cough, e.t.c)</option>
                 <option>Teens and children sickness</option>
                 <option>Cancer problems</option>
                 <option>Skin diseases</option>
@@ -150,15 +203,43 @@ const Form = props => {
                 <option>Respiratory problems</option>
                 <option>Eye problems</option>
                 <option>Bone and muscle problems</option>
-                </select>
-              </div>
-  
-              <button type="submit">Book</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-}
+              </select>
+            </div>
 
-export default Form
+            <div>
+              <label>Gender</label>
+              <select value={gender} onChange={handleGenderChange}>
+                <option>Select</option>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
+          </div>
+
+          <div
+            style={{
+              width: "100%",
+              textAlign: "right",
+              padding: "1rem",
+              boxSizing: "border-box",
+            }}
+          >
+            <button type="submit" className="book">
+              Book appointment
+            </button>
+          </div>
+        </form>
+      )}
+      {sucess && (
+        <div style={{ textAlign: "center" }}>
+          <img src={Success} height="40%" width="40%" alt="" />
+          <p>
+            Your booking was successful. Check your email form more information
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Form;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,92 +1,150 @@
+import { Fragment, useState } from "react";
 import Form from "../Form/Form";
-import Banner from '../../images/banner.png';
-import Logo from '../../images/blossom heart.png';
-import Medicine from '../../images/medicine.png'
+import Modal from "../Modal/Modal";
 
-import './Home.css'
+import Banner from "../../images/banner.png";
+import Logo from "../../images/blossom heart.png";
+import Medicine from "../../images/medicine.png";
+import Lab from "../../images/lab.png";
+import Firstaid from "../../images/first-aid.png";
+import Nurse from "../../images/nurse.png";
+import Avatar from "../../images/avatar.png";
 
-const Home = props => {
-    return(
-        <div className="container">
-            <header>
-                <div className="logo-div">
-                <img src={Logo} className="logo" />
-                <h3>Blossom Heart Hospital</h3>
-                </div>
+import "./Home.css";
 
-              
-                <ul clasName="nav">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Nav</li>
-                </ul> 
-            </header>
+const Home = (props) => {
+  const [form, showForm] = useState(false);
 
-            <div className="banner-div">
-                <div className="banner-desc">
-                    <h3>Virtual Health care for you</h3>
-                    <p>
-                    In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate
-                     the visual form of a document or a typeface without relying on meaningful content. 
-                    Lorem ipsum may be used as a placeholder before final copy is available.
-                    </p>
-                    <button className="action">Book appointment</button>
-                </div>
+  const showFormHandler = () => {
+    showForm(true);
+  };
 
-                <div className="banner-img">
-                    <img src={Banner} className="banner" alt="blossom-banner"/>
-                </div>
-            </div>
+  const hideFormHandler = () => {
+    showForm(false);
+  };
 
-            <div className="service">
-                <h3>Our services</h3>
-                <p>
-                In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate
-                the visual form of a document or a typeface without relying on meaningful content. 
-                Lorem ipsum may be used as a placeholder before final copy is available.
-                </p>
+  return (
+    <Fragment>
+      <div className="container">
+        <header>
+          <div className="logo-div">
+            <img src={Logo} className="logo" alt="" />
+            <h3>Blossom Heart Hospital</h3>
+          </div>
 
-                <div className="sev-services">
-                    <div className="card">
-                      <img src={Medicine} alt="medicine-jar" />
-                      <h5>Online pharmacy</h5>
-                      <p>
-                         In publishing and graphic design, Lorem ipsum is a placeholder
-                         text commonly used to demonstrate the
-                      </p>
-                    </div>
-                    <div className="card">
-                    <img src={Medicine} alt="medicine-jar" />
-                      <h5>Online pharmacy</h5>
-                      <p>
-                         In publishing and graphic design, Lorem ipsum is a placeholder
-                         text commonly used to demonstrate the
-                      </p>
-                    </div>
-                    <div className="card">
-                    <img src={Medicine} alt="medicine-jar" />
-                      <h5>Online pharmacy</h5>
-                      <p>
-                         In publishing and graphic design, Lorem ipsum is a placeholder
-                         text commonly used to demonstrate the
-                      </p>
-                    </div>
-                    <div className="card">
-                    <img src={Medicine} alt="medicine-jar" />
-                      <h5>Online pharmacy</h5>
-                      <p>
-                         In publishing and graphic design, Lorem ipsum is a placeholder
-                         text commonly used to demonstrate the
-                      </p>
-                    </div>
-                </div>
-            </div>
+          <ul className="nav">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Nav</li>
+          </ul>
+        </header>
 
-            
-          
+        <div className="banner-div">
+          <div className="banner-desc">
+            <h3>Virtual Health care for you</h3>
+            <p>
+              In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the visual form of a document or
+              a typeface without relying on meaningful content. Lorem ipsum may
+              be used as a placeholder before final copy is available.
+            </p>
+            <button className="action" onClick={showFormHandler}>
+              Book appointment
+            </button>
+          </div>
+
+          <div className="banner-img">
+            <img src={Banner} className="banner" alt="blossom-banner" />
+          </div>
         </div>
-    )
-}
+
+        <div className="service">
+          <h3>Our services</h3>
+          <p>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before final copy is available.
+          </p>
+
+          <div className="card-container">
+            <div className="card">
+              <img src={Medicine} alt="medicine-jar" />
+              <h4>Online pharmacy</h4>
+              <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly used to demonstrate the
+              </p>
+            </div>
+            <div className="card">
+              <img src={Lab} alt="medicine-jar" />
+              <h4>Consultation</h4>
+              <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly used to demonstrate the
+              </p>
+            </div>
+            <div className="card">
+              <img src={Firstaid} alt="medicine-jar" />
+              <h4>Emergency care</h4>
+              <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly used to demonstrate the
+              </p>
+            </div>
+            <div className="card">
+              <img src={Medicine} alt="medicine-jar" />
+              <h4>Online pharmacy</h4>
+              <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly used to demonstrate the
+              </p>
+            </div>
+            <div style={{ width: "100%", textAlign: "center" }}>
+              <button className="learn">Learn more</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="about">
+          <img src={Nurse} alt="nurses" />
+          <div className="about-us">
+            <h4>Leading health care Providers</h4>
+            <p>
+              In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the
+            </p>
+            <p>
+              <button className="learn">Learn more</button>
+            </p>
+          </div>
+        </div>
+
+        <div className="footer">
+          <h2>What our customers are saying</h2>
+          <div className="feed">
+            <figure>
+              <img src={Avatar} alt="" />
+              <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+            </figure>
+            <p style={{ textAlign: "left", width: "40%" }}>
+              "In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the In publishing and graphic
+              design, Lorem ipsum is a placeholder text commonly used to
+              demonstrate the"
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {form && (
+        <Modal onClose={hideFormHandler}>
+          <Form onClose={hideFormHandler} />
+        </Modal>
+      )}
+    </Fragment>
+  );
+};
 
 export default Home;
